641e185e938cbd179be16fb39846c18e95ad1433
DmitriyKrekshin <dima.krejshin@gmail.com>
2025-02-09 15:03:10 +0300

    Create IPersonService.cs

---
+++ BUKEP.CommunityFund.Domain/Interfaces/Services/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace BUKEP.CommunityFund.Domain;
+
+public interface IPersonService
+{
+    PersonEntity AddPerson(PersonEntity person);
+    
+    PersonEntity EditPerson(PersonEntity person);
+
+    void ExpelPerson(Guid personGuid);
+    
+    void ReadmitPerson(Guid personGuid);
+    
+    IEnumerable<PersonEntity> GetPersons(Expression<Func<PersonEntity, bool>> predicate);
+}
